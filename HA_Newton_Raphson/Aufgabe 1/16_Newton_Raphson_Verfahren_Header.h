2dd15d505189ae73a21b49ace823cc2aa50e34ae
inf20035 <75973281+inf20035@users.noreply.github.com>
2020-12-18 15:59:57 +0100

    Delete 16_Newton_Raphson_Verfahren_Header.h

--- HA_Newton_Raphson/Aufgabe 1/16_Newton_Raphson_Verfahren_Header.h
+++
@@ -1,8 +0,0 @@
-/*Die ausgelagerte Funktion wird mit ihren Parametern und ihrem Rückgabetyp deklariert*/
-
-#ifndef NEWTON_RAPHSON_VERFAHREN_FUNKTIONEN
-#define NEWTON_RAPHSON_VERFAHREN_FUNKTIONEN
-
-void Newton_Raphson_Verfahren(double*, double*, double*);
-
-#endif
